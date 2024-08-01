d4a5aeb01299206523665c2c6fc9fa5b34536a62
Radomirikus <r2a3d0o3m1i2r@gmail.com>
2024-08-01 17:57:36 +0300

    высушил код

--- hh.py
+++ hh.py
@@ -1,7 +1,7 @@
 import requests
 from itertools import count
 
-def predict_rub_salary_HH(salary):
+def predict_rub_salary(salary):
     if salary['from'] and salary['to']:
         return salary['from'] + salary['to'] / 2
 
@@ -20,27 +20,29 @@ def get_HH_statistic():
     for language in languages:
         vacancies_processed = 0
         salary_sum = 0
+        per_page = 100
+        town = 1
         for page in count(0, 1):
             payload = {
                 "text" : language,
-                "area" : 1,
+                "area" : town,
                 "page" : page,
-                "per_page" : 100
+                "per_page" : per_page
             }
             response = requests.get(url, params=payload)
             response.raise_for_status()
-            if page >= response.json()['pages'] - 1:
+            platform_answer = response.json()
+            if page >= platform_answer['pages'] - 1:
                 break
-            for vacancy in response.json()['items']:
+            for vacancy in platform_answer['items']:
                 salary = vacancy['salary']
-                if salary is not None and salary['currency'] == 'RUR':
-                    predict_rub_salary_HH(salary)
+                if salary and salary['currency'] == 'RUR':
                     vacancies_processed += 1
-                    salary_sum += predict_rub_salary_HH(salary)
+                    salary_sum += predict_rub_salary(salary)
     
         average_salary = salary_sum / vacancies_processed
         languages_statistic[language] = {
-            "vacancies_found" : response.json()['found'],
+            "vacancies_found" : platform_answer['found'],
             "vacancies_processed" : vacancies_processed,
             "average_salary" : average_salary
         }
